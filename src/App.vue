d4850bbace009c616fbd4abf75a45465cb26c604
LêCôngMinh <minhlc@zent.vn>
2022-07-11 22:34:14 +0700

    update data

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,24 @@
 <template>
   <div id="app">
-    <!-- <HelloWorld/> -->
-    <!-- <LoginForm/> -->
-    <!-- <LoginFormNext/> -->
-    <!-- <CalculatorNew/>  -->
-    <!-- <ArrayNew/> -->
-    <!-- <PostProduct/> -->
-    <!-- <TestFrom/> -->
-    <!-- <TestOne/> -->
-    <ToDoList/>
+    <ProDuctIem/>
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue';
 // import LoginForm from './components/LoginForm.vue';
-// import TestFrom from './components/TestFrom.vue';
+// import TestBai1 from './components/bai4/TestBai1.vue';
 // import LoginFormNext from './components/LoginFormNext.vue';
 // import CalculatorNew from './components/CalculatorNew.vue'
 // import PostProduct from './components/PostProduct.vue'
 // import TestOne from './components/TestOne.vue'
-import ToDoList from './components/bai3/ToDoList.vue'
+import ProDuctIem from './components/bai3/ProDuctIem.vue'
 
 
 export default {
   name: 'App',
   components: {
-    // TestOne
-    // HelloWorld,
-    // PostProductm,
-    // LoginForm
-    // LoginFormNext,
-    // CalculatorNew,
-    // ArrayNew
-    // TestFrom
-    ToDoList
+    ProDuctIem
 },
   
 }
---
+++ src/components/bai4/TestBai1.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div class="container" ref="message"> 
+      <input type="number" v-model="data" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      data:''
+    }
+  },
+  methods: {
+    
+  },
+  watch: {
+       data (value) {
+           if(!value) {
+             this.$refs.message.style.backgroundColor = 'white';
+           }
+           if(value > 0 && value <10) {
+              this.$refs.message.style.backgroundColor = 'gray';
+           }else if(value >10 && value <=30) {
+              this.$refs.message.style.backgroundColor = 'green';
+           }else if(value >30 && value <= 50) {
+              this.$refs.message.style.backgroundColor = 'blue';
+           }else if(value >50 && value <=70) {
+              this.$refs.message.style.backgroundColor = 'violet';
+           }else if(value >70 && value <=90) {
+              this.$refs.message.style.backgroundColor = 'orange';
+           }else if(value >90 && value <=100) {
+              this.$refs.message.style.backgroundColor = 'red';
+           }
+       }
+   }
+}
+</script>
+<style>
+ .container {
+  border: 1px solid #333;
+  background-color:white;
+ }
+</style>
